2e3afabe5e315ff8338e94edbf9c64327d43ccee
sahirvirmani <sahirvirmani@gmail.com>
2017-08-06 16:00:58 +0530

    New Glacier Landscape
    Added some cool shizz

--- VWorld/Assets/_Scripts/CameraFreeLook.cs
+++ VWorld/Assets/_Scripts/CameraFreeLook.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class CameraFreeLook : MonoBehaviour {
 
-	Camera mainCamera;
-
 	public float smoothSpeed;
 	public float moveSpeed;
 	public float speedMultiplier = 1;
 
 	void Start() {
-		mainCamera = Camera.main;
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 		
@@ -28,11 +25,11 @@ public class CameraFreeLook : MonoBehaviour {
 		float x = Input.GetAxis ("Mouse X");
 		float y = Input.GetAxis ("Mouse Y");
 
-			mainCamera.transform.position = Vector3.Lerp (mainCamera.transform.position, mainCamera.transform.position + (mainCamera.transform.forward * v) + (mainCamera.transform.right * h), Time.deltaTime * smoothSpeed);
+			transform.position = Vector3.Lerp (transform.position, transform.position + (transform.forward * v) + (transform.right * h), Time.deltaTime * smoothSpeed);
 
 		Vector3 newOrientation = transform.eulerAngles + new Vector3 (-y, x, 0);
 
-		mainCamera.transform.eulerAngles = newOrientation;
+		transform.eulerAngles = newOrientation;
 
 	}
 }
