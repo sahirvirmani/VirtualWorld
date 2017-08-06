2e57aad818c9b8730e11bcf81793ea7cb97f6b34
sahirvirmani <sahirvirmani@gmail.com>
2017-08-06 14:31:43 +0530

    Modified Water Environment

---
+++ VWorld/Assets/_Scripts/CameraFreeLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFreeLook : MonoBehaviour {
+
+	Camera mainCamera;
+
+	public float smoothSpeed;
+	public float moveSpeed;
+	public float speedMultiplier = 1;
+
+	void Start() {
+		mainCamera = Camera.main;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftShift))
+			speedMultiplier = 5;
+		else
+			speedMultiplier = 1;
+
+		float h = Input.GetAxis ("Horizontal") * moveSpeed * speedMultiplier;
+		float v = Input.GetAxis ("Vertical") * moveSpeed * speedMultiplier;
+
+		float x = Input.GetAxis ("Mouse X");
+		float y = Input.GetAxis ("Mouse Y");
+
+			mainCamera.transform.position = Vector3.Lerp (mainCamera.transform.position, mainCamera.transform.position + (mainCamera.transform.forward * v) + (mainCamera.transform.right * h), Time.deltaTime * smoothSpeed);
+
+		Vector3 newOrientation = transform.eulerAngles + new Vector3 (-y, x, 0);
+
+		mainCamera.transform.eulerAngles = newOrientation;
+
+	}
+}
---
+++ VWorld/Assets/_Scripts/oceanGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oceanGenerator : MonoBehaviour {
+
+	public GameObject Waves;
+
+	Transform[] posArray;
+
+	// Use this for initialization
+	void Start () { 
+		posArray = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			posArray[i] = transform.GetChild (i);
+		}
+		foreach(Transform p in posArray) {
+			Debug.Log ("p is at " + p.position.x);
+			Instantiate (Waves, p.position, p.rotation, p);
+		}
+	}
+}
