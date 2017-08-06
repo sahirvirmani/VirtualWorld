4c039f24db0679f1843fe1dfbeca1876c0334fac
sahirvirmani <sahirvirmani@gmail.com>
2017-08-06 19:59:54 +0530

    Fire Landscape Added

---
+++ VWorld/Assets/CameraManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	public Camera waterCamera;
+	public Camera earthCamera;
+	public Camera airCamera;
+	public Camera fireCamera;
+
+	public void Start() {
+		DisableCamera ();
+		waterCamera.enabled = true;
+	}
+
+	public void DisableCamera() {
+		waterCamera.enabled = false;
+		earthCamera.enabled = false;
+		airCamera.enabled = false;
+		fireCamera.enabled = false;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			waterCamera.enabled = true;
+			fireCamera.enabled = false;
+			earthCamera.enabled = false;
+			airCamera.enabled = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			waterCamera.enabled = false;
+			fireCamera.enabled = true;
+			earthCamera.enabled = false;
+			airCamera.enabled = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			waterCamera.enabled = false;
+			fireCamera.enabled = false;
+			earthCamera.enabled = true;
+			airCamera.enabled = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			waterCamera.enabled = false;
+			fireCamera.enabled = false;
+			earthCamera.enabled = false;
+			airCamera.enabled = true;
+		}
+	}
+}
---
+++ VWorld/Assets/Lava.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+
+	Renderer renderer;
+	public Color mainColor;
+	public Color specColor;
+
+	void Start() {
+		renderer = GetComponent<Renderer> ();
+		renderer.material.SetColor ("_Color", mainColor);
+		renderer.material.SetColor ("_SpecColor", specColor);
+		renderer.material.SetFloat ("_WaveLength", 0.1f);
+		renderer.material.SetFloat ("_WaveHeight", 0.1f);
+	}
+}
